6f9efeac78b5f0ab8dc03634309473d3df6954f3
Apfeltee96 <kerem.ac@hotmail.de>
2026-03-26 15:29:11 +0100

    Build-Ordner aus Git entfernt und gitignore fixiert

--- build/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/kerem/Desktop/C++Projekt/CarRace/raylib/src
-# Build directory: C:/Users/kerem/Desktop/C++Projekt/CarRace/build/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
