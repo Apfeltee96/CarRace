04fda846299e9e9b0e78ea5425b2d69c5922485a
Apfeltee96 <kerem.ac@hotmail.de>
2026-03-28 12:53:26 +0100

    beim start terminal ausgeblendet

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#if defined(_WIN32) && !defined(_DEBUG)
+    #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+
 #define _CRT_SECURE_NO_WARNINGS
 #include "raylib.h"
 #include "scoreboard.h"
